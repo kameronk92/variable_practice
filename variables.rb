22cd82875a7c75599bc8a1882845f3390a7401bf
Kam Kennedy <kameronk013@gmail.com>
2023-07-08 16:06:23 -0600

    relabels several variables and adds printout

--- variables.rb
+++ variables.rb
@@ -8,5 +8,13 @@ restaurant_seats = 120
 cafe_seats = 22
 bar_seats = 76
 restaurant_open = false
-bar _open = false
+bar_open = false
 cafe_open = true
+restaurant_name = "Chochkis"
+cafe_name = "Mocha Joe's"
+bar_name = "Cheers"
+
+puts bar_name
+puts bar_rating
+puts bar_open
+puts bar_seats
